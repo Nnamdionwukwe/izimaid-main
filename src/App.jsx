0ae3ff4768c8228b30d304c8256567896f765934
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-14 15:38:24 +0100

    f fes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./component/HomePage";
 import RequestEstimate from "./component/RequestEstimate";
 import LearnMore from "./component/LearnMore/LearnMore";
+import Login from "./component/Login";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<HomePage />} />
 
-        <Route index path="/login" element={<HomePage />} />
+        <Route index path="/login" element={<Login />} />
 
         <Route path="/request-a-free-estimate" element={<RequestEstimate />} />
 
