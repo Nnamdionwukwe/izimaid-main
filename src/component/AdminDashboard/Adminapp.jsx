54d5be94d0f38225e94ef0542ca63aa634f8cc2a
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-16 00:51:46 +0100

    dscvds

--- src/component/AdminDashboard/Adminapp.jsx
+++ src/component/AdminDashboard/Adminapp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import AdminDashboard from "./AdminDashboard.jsx";
-import AdminUsers from "./AdminUsers.jsx";
+import AdminUsers from "./Adminusers.jsx";
 
 export default function AdminApp({ onLogout }) {
   const [view, setView] = useState("leads"); // "leads" | "users"
