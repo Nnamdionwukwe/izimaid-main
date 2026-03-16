759bd1b9a72ed8acd236abfbffb4db495fcb6c43
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-16 01:12:32 +0100

    dcwd

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-      "Cross-Origin-Embedder-Policy": "unsafe-none",
-    },
-  },
+  // server: {
+  //   headers: {
+  //     "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+  //     "Cross-Origin-Embedder-Policy": "unsafe-none",
+  //   },
+  // },
 });
