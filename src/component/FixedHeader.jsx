3d5318ae00e4d92d031a9295845e11c4f6b5e03e
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-19 17:39:38 +0100

    wdcew

--- src/component/FixedHeader.jsx
+++ src/component/FixedHeader.jsx
@@ -6,24 +6,22 @@ export default function FixedHeader() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div className={styles.SubHeader1}>
-        <a href="https://deusizisparkle.com">
-          <img className={styles.logo} alt="Logo" src="izimaid.jpg" />
-        </a>
+    <div className={styles.SubHeader1}>
+      <a href="https://deusizisparkle.com">
+        <img className={styles.logo} alt="Logo" src="izimaid.jpg" />
+      </a>
 
-        <div className={styles.hamburger}>
-          <i
-            onClick={() => setIsOpen((is) => !is)}
-            class="fa fa-bars"
-            aria-hidden="true"
-          ></i>
+      <div className={styles.hamburger}>
+        <i
+          onClick={() => setIsOpen((is) => !is)}
+          class="fa fa-bars"
+          aria-hidden="true"
+        ></i>
 
-          <div className={styles.sideBarMain}>
-            {isOpen && <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />}
-          </div>
+        <div className={styles.sideBarMain}>
+          {isOpen && <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />}
         </div>
       </div>
-    </>
+    </div>
   );
 }
