c1f8e73b9a1815d64187c3fe28ed6d5de4b1c9a1
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-19 23:46:58 +0100

    fsdv fs

--- src/component/LightCommercialSideBar.jsx
+++ src/component/LightCommercialSideBar.jsx
@@ -1,20 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./ResidentialSideBar.module.css";
 
 export default function LightCommercialSideBar() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className={styles.residentialDI}>
-        <div className={styles.hoverMainDiv1}>
+        <div
+          onClick={() => navigate("/recurring-cleaning")}
+          className={styles.hoverMainDiv1}
+        >
           <i class="fa fa-th-large" aria-hidden="true"></i>
           <p>Recurring Cleaning</p>
         </div>
 
-        <div className={styles.hoverMainDiv2}>
+        <div
+          onClick={() => navigate("/One-time-cleaning")}
+          className={styles.hoverMainDiv2}
+        >
           <i class="fa fa-th-large" aria-hidden="true"></i>
           <p>One Time Cleaning</p>
         </div>
 
-        <div className={styles.hoverMainDiv6}>
+        <div
+          onClick={() => navigate("occasional-cleaning")}
+          className={styles.hoverMainDiv6}
+        >
           <i class="fa fa-th-large" aria-hidden="true"></i>
           <p>Occasional Cleaning</p>
         </div>
