8f27c4305bfb26187338b9fee443b5c25fc81bf2
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-20 15:55:00 +0100

    fdvds

--- src/component/Customers.jsx
+++ src/component/Customers.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Customers.module.css";
 
 export default function Customers() {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.customers}>
       <div>
@@ -8,7 +11,7 @@ export default function Customers() {
         <h2>Over 100k customers served since 2025</h2>
       </div>
 
-      <h4>Learn More</h4>
+      <h4 onClick={() => navigate("/our-results")}>Learn More</h4>
     </div>
   );
 }
